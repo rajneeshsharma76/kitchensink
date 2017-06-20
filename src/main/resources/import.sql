5f827322cba23cba13c7f56325fd1694df86cbb3
rajneeshsharma76 <rajneeshandroid@gmail.com>
2017-06-20 16:48:02 +0530

    Testing name
    Testing name

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -16,4 +16,4 @@
 --
 
 -- You can use this file to load seed data into the database using SQL statements
-insert into Member (id, name, email, phone_number) values (0, 'Rajneesh', 'Rajneesh.sharma@gspann.com', '9811730955') 
+insert into Member (id, name, email, phone_number) values (0, 'Rajneesh kumar', 'Rajneesh.sharma@gspann.com', '9811730955') 
